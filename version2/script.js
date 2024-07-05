2f988dddcf2ac76cdf6f59ca5c1a496f5b32dee2
lxeso <seolseee@gmail.com>
2024-07-05 12:17:39 +0900

    오타 수정

--- version2/script.js
+++ version2/script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     // 로컬 스토리지에서 할 일 목록을 불러옵니다. 
     //DOMContentLoaded 이벤트는 HTML 문서가 완전히 로드되고 파싱된 후에 발생합니다. 이 이벤트가 발생하면 할 일 목록 기능을 초기화합니다
-    const todos = JSON.parse(localStorage.getItem("todos")) || []; // 컬 스토리지에서 할 일 목록을 불러옵니다. 로컬 스토리지에 저장된 데이터가 없으면 빈 배열을 사용합니다.
-    const todoList = document.getElementById("todo-list"); // 할 일 목록을 표시할 요소를 가져옵니다.
+    const todos = JSON.parse(localStorage.getItem("todos")) || []; // 로컬 스토리지에서 할 일 목록을 불러옵니다. 로컬 스토리지에 저장된 데이터가 없으면 빈 배열을 사용합니다.
+    const todoList = ("todo-list"); // 할 일 목록을 표시할 요소를 가져옵니다.
     const todoInput = document.getElementById("todo-input");
     const addTodoButton = document.getElementById("add-todo");
 
